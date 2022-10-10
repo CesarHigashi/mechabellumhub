8175366c2656462a9ae05a83cf410d3de1330ef6
Higashi <seigi.higashi@gmail.com>
2022-10-10 16:50:19 -0500

    Cosas para Assets y Autentificación
    CLase de 10/10/2022

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,13 @@ Route::get('/', function () {
 
 /* RUTA PARA TODOS LOS METODOS */
 Route::resource('plane', PlaneController::class);
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
