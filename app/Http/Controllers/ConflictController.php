2b8d4c2cbd66d39aa9546349392ad6e7cd7c099e
Higashi <seigi.higashi@gmail.com>
2022-10-26 21:27:19 -0500

    Creación de elementos para 'Conflict' y 'Nation'
    Cree el modelo, resources, migración, seeder y factory para los conflictos y las naciones

---
+++ app/Http/Controllers/ConflictController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Conflict;
+use Illuminate\Http\Request;
+
+class ConflictController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Conflict  $conflict
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Conflict $conflict)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Conflict  $conflict
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Conflict $conflict)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Conflict  $conflict
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Conflict $conflict)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Conflict  $conflict
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Conflict $conflict)
+    {
+        //
+    }
+}
