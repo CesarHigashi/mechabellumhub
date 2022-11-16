d9067ec8157497103d21a9e654fb992a76016bbf
Osvaldo González <gonzalezascencioosvaldo@outlook.com>
2022-11-16 01:57:17 -0600

    update: Index aviones y tanques add: imagenes_ejemplo
    Actualicé los Index de aviones y de tanques para que se vea como una galería, también añadí imágenes como ejemplo para dicha galería.

--- resources/views/plane/planeIndex.blade.php
+++ resources/views/plane/planeIndex.blade.php
@@ -5,6 +5,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Listado de aviones</title>
+
+    <!-- RUTAS Y ESTILOS PROVISIONALES-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="/styles/source/bootstrap-3.3.6-dist/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="/styles/source/font-awesome-4.5.0/css/font-awesome.css">
+	<link rel="stylesheet" type="text/css" href="/styles/style/slider.css">
+	<link rel="stylesheet" type="text/css" href="/styles/style/mystyle.css">
+	<link rel="stylesheet" type="text/css" href="/styles/style/formstyle.css">
+    
 </head>
 <body>
     <h1>Listado de aviones registrados</h1>
@@ -44,3 +53,30 @@
     </ul>
 </body>
 </html>
+
+<!-- GALERIA DE VEHICULOS | ¿PROVISIONAL? -->
+
+<div class="latestcars">
+    <h1 class="text-center">&bullet; AVIONES &bullet;</h1>
+</div>
+
+<div class="grid">
+    <div class="row">
+        @foreach ($planes as $plane)
+            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+                <div class="txthover">
+                    <img src="/styles/image/B17.jpg" alt="B-17">
+                    <div class="txtcontent">
+                        <div class="stars"></div>
+                        <div class="simpletxt">
+                            <h3 class="name">{{ $plane->name }} </h3>
+                            <p> {{ $plane->country }} </p>
+                            <h4 class="price"> {{ $plane->category }} </h4>
+                            <a class="" href="/plane/{{ $plane->id }}">LEER MÁS</a><br>    
+                        </div>
+                    </div>
+                </div>	 
+            </div>
+        @endforeach
+    </div>	 
+</div>
--- resources/views/tank/tankIndex.blade.php
+++ resources/views/tank/tankIndex.blade.php
@@ -5,6 +5,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Listado de tanques</title>
+
+    <!-- RUTAS Y ESTILOS PROVISIONALES-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="/styles/source/bootstrap-3.3.6-dist/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="/styles/source/font-awesome-4.5.0/css/font-awesome.css">
+	<link rel="stylesheet" type="text/css" href="/styles/style/slider.css">
+	<link rel="stylesheet" type="text/css" href="/styles/style/mystyle.css">
+	<link rel="stylesheet" type="text/css" href="/styles/style/formstyle.css">
+
 </head>
 <body>
     <h1>Listado de tanques registrados</h1>
@@ -42,3 +51,30 @@
     </ul>
 </body>
 </html>
+
+<!-- GALERIA DE VEHICULOS | ¿PROVISIONAL? -->
+
+<div class="latestcars">
+    <h1 class="text-center">&bullet; TANQUES &bullet;</h1>
+</div>
+
+<div class="grid">
+    <div class="row">
+        @foreach ($tanks as $tank)
+            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+                <div class="txthover">
+                    <img src="/styles/image/M4-Sherman.png" alt="M4-Sherman">
+                    <div class="txtcontent">
+                        <div class="stars"></div>
+                        <div class="simpletxt">
+                            <h3 class="name">{{ $tank->name }} </h3>
+                            <p> {{ $tank->country }} </p>
+                            <h4 class="price"> {{ $tank->category }} </h4>
+                            <a class="" href="/tank/{{ $tank->id }}">LEER MÁS</a><br>    
+                        </div>
+                    </div>
+                </div>	 
+            </div>
+        @endforeach
+    </div>	 
+</div>
